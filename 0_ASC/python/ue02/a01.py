7e3a12132a7cfe120eff8e7e399d53b6a8ba6dc3
poel22 <hoermannpaul@gmail.com>
2019-11-12 07:55:06 +0100

    add asc and bgd

--- 0_ASC/python/ue02/a01.py
+++ 0_ASC/python/ue02/a01.py
@@ -3,6 +3,9 @@ import urllib.request
 import random
 import gzip
 import re
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
 
 filePath = "./ftpSourceView.txt";
 
@@ -14,22 +17,32 @@ print("URL: " + sourceUrl)
 unzippedFiles = []
 
 
-for i in range(1000):
+for i in range(100):
     data = {}
     fullUrl = sourceUrl + fileContent[2:][random.randint(0, len(fileContent) - 2)].split('"')[1] # faster than r"(\".*\")"
     content = gzip.GzipFile(fileobj=urllib.request.urlopen(fullUrl)).readlines()
     split = content[0].split()
-    data["header"] = split[1]
-    data["pdbid"] = split[3]
-    for line in content[1:]:
+    data["header"] = split[1].decode()
+    data["pdbid"] = split[3].decode()
+    data["X"] = 0
+    data["Y"] = 0
+    data["Z"] = 0
+    data["title"] = []
+    for line_src in content[1:]:
+        line = line_src.decode()
         match = re.match(r"TITLE\s\d?\s?(.*)", line)
         if match != None:
-            data["title"] = match.group(1)
+            data["title"].append(match.group(1).strip())
         elif line.startswith("ATOM"):
             split = line.split();
-            # TODO
+            data["X"] += float(split[6])
+            data["Y"] += float(split[7])
+            data["Z"] += float(split[8])
         else:
             match = re.match(r".*CHAIN:\s(.*);", line)
-            data["chain"] = match.group(1)
-        print(data["title"])
+            if match != None:
+                data["chain"] = match.group(1)
+    data["title"] = " ".join(data["title"])
+    pp.pprint(data)
+
 
