de91a8a6e65120b91cfc6ac05c2a70a2052bb76d
hoermannpaul <hoermannpaul@gmail.com>
2019-11-17 22:07:44 +0100

    finish python ue02

--- 0_ASC/python/ue02/a02.py
+++ 0_ASC/python/ue02/a02.py
@@ -1,44 +1,85 @@
-import os
 import urllib.request
 import re
 import pprint
 
-pp = pprint.PrettyPrinter(indent=2)
+PP = pprint.PrettyPrinter(indent=1)
 
-base_url = "https://www.icd-code.de/icd/code/"
-starting_url = "ICD-10-GM.html"
-list_of_ids = [ "H", "J" ]
-tree = {}
+BASE_URL = "https://www.icd-code.de/icd/code/"
+STARTING_URL = "ICD-10-GM.html"
+LIST_OF_IDS = ["H", "J"]
+
 
 def build_tree(url, ids, base):
-    fileH = urllib.request.urlopen(url)
-    for j in fileH.readlines():
+    tree = {}
+    file = urllib.request.urlopen(url)
+    for j in file.readlines():
         for i in ids:
-            identification = re.search(r"<a href=\"(" + i + "\d\d-" + i + "\d\d.html)\">(\w\d\d-\w\d\d)<\/a><\/td><td>(.+?)<a", str(j))
-            if identification == None: continue
-            pp.pprint(identification.group(1))
-            pp.pprint(identification.group(2))
-            pp.pprint(identification.group(3))
-            key = identification.group(3)
+            identification = re.search(r"<a href=\"(" + i + r"\d\d-" + i +
+                                       r"\d\d.html)\">(\w\d\d-\w\d\d)<\/a><\/td><td>(.+?)<a",
+                                       str(j))
+            if identification is None:
+                continue
+            key = identification.group(2)
             tree[key] = {
                 "link": base + identification.group(1),
-                "detail": base + identification.group(3)
+                "detail": base + identification.group(3),
+                "more": parse_second_level(i, base, base + identification.group(1))
             }
-            pp.pprint(tree[key]["link"])
-            for l in urllib.request.urlopen(tree[key]["link"]).readlines():
-                # TODO
-                match1 = re.search(r"<a href=\"(\w\d\d-\w\d\d.html)\">(\w\d\d-\w\d\d)<\/a>(.+?)(<br>|<\/td>)", str(l))
-                if match1 == None: continue
-                pp.pprint(match1.group(1))
-                pp.pprint(match1.group(2))
-                pp.pprint(match1.group(3))
-                for line in urllib.request.urlopen(tree[key]["link"]).readlines():
-                    match2 = re.search(r"<a href=\"(" + i + "\d\d.-\*?.html)\">(" + i + "\d\d.-\*?)<\/a>(.+?)(<br>|<\/td>)", str(line))
-                    if match2 == None: continue
-                    pp.pprint(match2.group(1))
-                    pp.pprint(match2.group(2))
-                    pp.pprint(match2.group(3))
+    PP.pprint(tree)
+
+# def parse_first_level(url, ids, base, tree):
 
 
+def parse_second_level(cur_id, base, link):
+    tree = {}
+    for line in urllib.request.urlopen(link).readlines():
+        matches = re.findall(r"<a href=\"(" + cur_id +
+                             r"\d\d-\w\d\d.html)\">(\w\d\d-\w\d\d)<\/a>(.+?)(<br\/>|<\/td>)",
+                             str(line))
+        if matches is None:
+            continue
+        for match in matches:
+            key = match[1]
+            sublink = base + match[0]
+            tree[key] = {
+                "link": sublink,
+                "detail": match[2].strip(),
+                "more": parse_third_level(cur_id, base, sublink)
+            }
+    return tree
 
-build_tree(base_url + starting_url, list_of_ids, base_url)
+
+def parse_third_level(cur_id, base, link):
+    tree = {}
+    for line in urllib.request.urlopen(link).readlines():
+        matches = re.findall(r"<a href=\"(" + cur_id +
+                             r"\d\d(?:.-\*?)?.html)\">(" + cur_id +
+                             r"\d\d(?:.-\*?)?)<\/a>(.+?)(<br\/>|<\/td>)", str(line))
+        if matches is None:
+            continue
+        for match in matches:
+            key = match[1]
+            sublink = base + match[0]
+            tree[key] = {
+                "link": sublink,
+                "detail": match[2].strip(),
+                "more": parse_fourth_level(cur_id, base, sublink)
+            }
+    return tree
+
+
+def parse_fourth_level(cur_id, base, link):
+    tree = {}
+    for line in urllib.request.urlopen(link).readlines():
+        matches = re.findall(r"<tr><td valign=\"top\"><div class=\"code_bottom\">(" + cur_id +
+                             r"\d\d\.\d\d?(?:-?\*?)?)<\/div><\/td><td colspan=\"2\">(.+?)<\/td><\/tr>",
+                             str(line))
+        if matches is None:
+            continue
+        for match in matches:
+            key = match[0]
+            tree[key] = match[1].strip()
+    return tree
+
+
+build_tree(BASE_URL + STARTING_URL, LIST_OF_IDS, BASE_URL)
