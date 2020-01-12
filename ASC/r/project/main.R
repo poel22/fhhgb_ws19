f023f6890d936aa97930b7a7513f7426601d0f04
poel22 <hoermannpaul@gmail.com>
2020-01-12 17:52:01 +0100

    r project make progress

--- ASC/r/project/main.R
+++ ASC/r/project/main.R
@@ -24,3 +24,32 @@ unique(source$cons.conf.idx)
 unique(source$euribor3m)
 unique(source$nr.employed)
 unique(source$y)
+
+source = droplevels(source %>% filter(source$loan != 'unknown'))
+
+pie(prop.table(table(source$loan)))
+
+# Age distribution
+hsAge = hist(source$age)
+
+glimpse(source)
+
+fsMarital = droplevels(source %>% filter(source$marital != 'unknown'))
+
+barplot(prop.table(table(fsMarital$marital)))
+
+fsJob = droplevels(source %>% filter(source$job != 'unknown'))
+
+fsMarital$marital+loan
+
+pie(prop.table(table(fsJob$job)))
+
+unique(fsMarital$age)
+
+ks.test(x = hsAge$counts, y = 'pnorm')
+
+unique(fsMarital$nr.employed)
+
+pie(prop.table(table(source$nr.employed)))
+
+cSource = source %>% group_by(nr.employed) %>% add_count() %>% select(nr.employed, n) %>% plot_ly(sort = T, labels = source$nr.employed, values = "n") %>% add_pie()
---
+++ ASC/r/project/todo.md
@@ -0,0 +1,9 @@
+# TODO
+## Hypothesis tests
+ * employees
+ * job
+ * marital status
+## Grafics
+ * print overview showing how likely the loan is on the specific attributes
+ * pie chart on attributes
+ * how many yes and no
